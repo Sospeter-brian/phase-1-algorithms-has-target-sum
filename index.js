79dc102dfcaa9bab7cd524c6b811f1cedce573d8
sospeterbrian029@gmail.com <sospeterbrian029@gmail.com>
2022-06-18 18:22:41 +0300

    target sum

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
 }
+// O(n) runtime
+//function findSock(array) {
+  //for (const item of array) {
+    //if (item === "sock") return "sock";
+  //}
+//}
+
+// O(1) runtime
+//function findSock(object) {
+  //if (object.sock) return "sock";
+//}
 
 /* 
   Write the Big O time complexity of your function here
@@ -29,6 +50,24 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("")
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+console.log("")
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
+
+console.log("")
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
